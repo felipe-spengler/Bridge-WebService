c444058c0bbd2c4b51f5233804694275b5d7f955
Felipe Spengler <felipinhospengler@gmail.com>
2023-11-03 01:48:30 -0300

    Update fazConexao.php

--- fazConexao.php
+++ fazConexao.php
@@ -1,35 +1,39 @@
 <?php
-header('Content-Type: text/html charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 include 'conexao2.php';
 
 $resposta = array();
 $resposta["erro"] = true;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $login = "'".$_POST['login']."'";
-    $senha = "'".$_POST['senha']."'";
-    echo $login;
-    echo $senha;
+    if (isset($_POST['login']) && isset($_POST['senha'])) {
+        $login = $_POST['login'];
+        $senha = $_POST['senha'];
 
-     $resposta["login"] = $login;
-    $resposta["senha"] = $senha;
-    $conexao = conectarAoBanco();
-    if ($conexao === null) {
-        $resposta["mensagem"] = "Erro na conexão com o banco de dados.";
-    } else {
-        $sql = "SELECT cargo FROM funcionario WHERE loginfuncionario='$login' AND senhafuncionario='$senha'";
-        $result = $conexao->query($sql);
+        $conexao = conectarAoBanco();
 
-        if ($result) {
-            $row = $result->fetch_assoc();
-            $resposta["erro"] = false;
-            $resposta["cargo"] = $row["cargo"];
+        if ($conexao === null) {
+            $resposta["mensagem"] = "Erro na conexão com o banco de dados.";
         } else {
-             $resposta["cargo"] = "erro na query";
-            $resposta["mensagem"] = "Nada a mostrar";
-        }
+            $sql = "SELECT cargo FROM funcionario WHERE loginfuncionario = ? AND senhafuncionario = ?";
+            $stmt = $conexao->prepare($sql);
+            $stmt->bind_param("ss", $login, $senha);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        $conexao->close();
+            if ($result) {
+                $row = $result->fetch_assoc();
+                $resposta["erro"] = false;
+                $resposta["cargo"] = $row["cargo"];
+            } else {
+                $resposta["mensagem"] = "Erro na query: " . $conexao->error;
+            }
+
+            $stmt->close();
+            $conexao->close();
+        }
+    } else {
+        $resposta["mensagem"] = "Dados de login e senha não foram recebidos corretamente.";
     }
 } else {
     $resposta["mensagem"] = "Login e senha são obrigatórios.";
